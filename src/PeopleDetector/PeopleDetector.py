1b3da9d0509aba36bb0f12c9a88f4d7aaf089810
gnogueira <gnogueira@ciandt.com>
2016-03-02 14:18:10 -0300

    Criação inicial das classes de detecção de pessoas e objetos.

---
+++ src/PeopleDetector/PeopleDetector.py
@@ -0,0 +1,17 @@
+'''
+Created on 2 de mar de 2016
+
+@author: gnogueira
+'''
+
+class PeopleDetector(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
