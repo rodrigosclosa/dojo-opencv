b5eb2de9df033cc383e62cc68caef711295d5494
Rodrigo Sclosa <rodrigogs@ciandt.com>
2016-03-30 15:06:33 -0300

    Mais exemplos.

--- tests/template_matching.py
+++ tests/template_matching.py
@@ -4,12 +4,12 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 
 while(1):
-    _, frame = cap.read()
+    #_, frame = cap.read()
 
-    #img_rgb = cv2.imread('foto.jpg')
-    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    img_rgb = cv2.imread('foto.jpg')
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
-    template = cv2.imread('padrao3.jpg',0)
+    template = cv2.imread('padrao.jpg',0)
     w, h = template.shape[::-1]
 
     res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
@@ -17,9 +17,9 @@ while(1):
     loc = np.where( res >= threshold)
 
     for pt in zip(*loc[::-1]):
-        cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
 
-    cv2.imshow('Detected',frame)
+    cv2.imshow('Detected',img_rgb)
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
